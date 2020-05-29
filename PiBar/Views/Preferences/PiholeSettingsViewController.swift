413b21857533d63d410bbc3f0d14ef974380b2f5
Brad Root <bradroot@me.com>
2020-05-29 08:14:16 -0700

    add MPL notice to new files

--- PiBar/Views/Preferences/PiholeSettingsViewController.swift
+++ PiBar/Views/Preferences/PiholeSettingsViewController.swift
@@ -5,6 +5,9 @@
 //  Created by Brad Root on 5/25/20.
 //  Copyright © 2020 Brad Root. All rights reserved.
 //
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import Cocoa
 
