afe192a7799e5585344a6a59d57505decde89c43
Alexander Lindenstruth <dev@lindenstruth.org>
2025-06-23 10:18:53 +0200

    Store api tokens securely in the macOS Keychain
    Pibar is now storing api tokens securely in the macOS Login Keychain instead of using the UserDefaults for plain text storage. For this purpose an additional SPM dependency for a 3rd party keychain wrapper has been added.

--- PiBar/Data Sources/Structs.swift
+++ PiBar/Data Sources/Structs.swift
@@ -10,6 +10,7 @@
 //  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import Foundation
+import KeychainAccess
 
 // MARK: - Pi-hole Connections
 
@@ -81,37 +82,66 @@ extension PiholeConnectionV2 {
 }
 
 // PiBar v1.2 format
-struct PiholeConnectionV3: Codable {
+struct PiholeConnectionV3: Codable, Copyable {
     let hostname: String
     let port: Int
     let useSSL: Bool
-    let token: String
+    var token: String
     let passwordProtected: Bool
     let adminPanelURL: String
     let isV6: Bool
 }
 
 extension PiholeConnectionV3 {
+    
+    private var keychainIdentifier: String? {
+        get {
+            if self.hostname.count > 0 {
+                return "apitoken.\(self.hostname):\(self.port)"
+            }
+            return nil
+        }
+    }
+    
     init?(data: Data) {
         let jsonDecoder = JSONDecoder()
         do {
-            let object = try jsonDecoder.decode(PiholeConnectionV3.self, from: data)
+            var object = try jsonDecoder.decode(PiholeConnectionV3.self, from: data)
+            // load the token from the Keychain and add it to the connection object:
+            if let bundleIdentifier = Bundle.main.bundleIdentifier, let keychainIdentifier = object.keychainIdentifier {
+                let keychain = Keychain(service: bundleIdentifier, accessGroup: nil)
+                if let tokenStr = keychain[keychainIdentifier], !tokenStr.isEmpty {
+                    object.token = tokenStr
+                }
+            }
             self = object
         } catch {
             Log.debug("Couldn't decode connection: \(error.localizedDescription)")
             return nil
         }
     }
-
+    
     func encode() -> Data? {
         let jsonEncoder = JSONEncoder()
-        if let data = try? jsonEncoder.encode(self) {
+        // make sure the token is being securely stored to the Keychain only, and nowhere else
+        var copySelf = self.copy() as! PiholeConnectionV3
+        if let bundleIdentifier = Bundle.main.bundleIdentifier, !copySelf.token.isEmpty, let keychainIdentifier = copySelf.keychainIdentifier {
+            let keychain = Keychain(service: bundleIdentifier, accessGroup: nil)
+            keychain[keychainIdentifier] = copySelf.token
+            copySelf.token = ""
+        }
+        if let data = try? jsonEncoder.encode(copySelf) {
             return data
         } else {
             return nil
         }
     }
 
+    func copy(with zone: NSZone? = nil) -> Any {
+        let copy = PiholeConnectionV3(hostname: hostname, port: port, useSSL: useSSL, token: token, passwordProtected: passwordProtected, adminPanelURL: adminPanelURL, isV6: isV6)
+        return copy
+    }
+    
     static func generateAdminPanelURL(hostname: String, port: Int, useSSL: Bool) -> String {
         let prefix: String = useSSL ? "https" : "http"
         return "\(prefix)://\(hostname):\(port)/admin/"
